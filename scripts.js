82689dbae708ea9882ace01a744c47185d9de18b
behshad <82580731+biigii@users.noreply.github.com>
2023-03-09 21:00:24 +1300

    final more js

--- scripts.js
+++ scripts.js
@@ -1,32 +1,60 @@
-function button1Clicked() {
-  const img = document.querySelector('#image-1')
-  img.style.rotate = '45deg'
+// const num = 1000
+
+// if(num <= 100) {
+//   document.body.style.setProperty('background-color','#f11')
+// }
+
+// if (num > 100) {
+//   document.body.style.setProperty('background-color','#0f0')
+// }
+
+// if(age === 10) {
+//   console.log('age is 10')
+// } else {
+//   console.log('age is something else')
+// }
+
+
+function windowResized() {
+  const myBrowserWidth = window.innerWidth
+  if(myBrowserWidth > 1200) {
+    document.body.style.setProperty('background-color','#00f')
+  } else {
+    document.body.style.setProperty('background-color','#0f0')
+  }
 }
 
-document.querySelector('[data-button]').addEventListener('click', button1Clicked)
+// function moveBall(event) {
+//   const top = event.x
+//   const left = event.y
+//   const ball = document.querySelector('[data-ball]')
+//   console.log(top)
+//   //  top: 200px
+//   // console.log(top+'px')
+//   ball.style.top = top + 'px'
+//   ball.style.left = left + 'px' 
+// }
 
-function button2Click() {
-  document.body.style.setProperty('background-color','#0f0')
+function moveBall(event) {
+  const x = event.x
+  const y = event.y
+  const ball = document.querySelector('[data-ball]')
+  // ball.style.translate = '400px 300px'
+  // transform: translate(200px, 100px);
+ ball.style.transform = `translate(${x-50}px, ${y-50}px)`
 }
 
-function textEntered() {
-   let text = document.querySelector('[data-text]').value;
-   const heading = document.querySelector('[data-entry]')
-   heading.textContent = text
-}
+document.addEventListener('click', moveBall)
 
-function dataEntered() {
-  const input = document.querySelector('#fruit-text');
-  const val = input.value;
-  // const value = document.querySelector('fruit-text').value
-  const li = document.createElement('li')
-  li.innerHTML = val
-  const ul = document.querySelector('#fruit-list')
-  ul.appendChild(li)
-}
+// const num = 200
 
-function letterTyped() {
-    const text = document.querySelector('[data-letters]').value
-    const h2Text = document.querySelector('#h2-text')
-    h2Text.textContent = text
-}
+// console.log(num+'px')
+// console.log(`${num}px`)
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 100, 101]
+
+numbers.forEach(function(number){
+  const img = document.createElement('img')
+  img.src = 'https://picsum.photos/id/' + number + '/200'
+  document.body.appendChild(img)
+})
